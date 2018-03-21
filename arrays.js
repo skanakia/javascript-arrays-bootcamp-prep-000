89d62df650de1850bc31d5be0eb92cd0f018220d
Sameer Kanakia <skanakia1@gmail.com>
2018-03-21 17:47:32 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,12 @@
+var chocolateBars = [
+  'snickers',
+  'hundred grand',
+  'kitkat',
+  'skittles'
+]
+
+function addElementToBeginningOfArray(array, element) {
+  var newarray = array.unshift(element)
+} 
+
+function destructivelyAddElementToBeginningOfArray(array, element) { var newarray = array 
