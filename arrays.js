9f4026c6be725fcd6a1fe614cca313b60218021f
Sameer Kanakia <skanakia1@gmail.com>
2018-03-21 18:42:01 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -37,5 +37,10 @@ function removeElementFromBeginningOfArray(array) {
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
-  
+  array.pop()
+  return array
+}
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length - 1)
 }
