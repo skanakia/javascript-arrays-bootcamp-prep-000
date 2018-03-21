971cf84e045536b90418028722a379b9e38ce2a1
Sameer Kanakia <skanakia1@gmail.com>
2018-03-21 18:11:39 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -10,5 +10,5 @@ function addElementToBeginningOfArray(array, element) {
 } 
 
 function destructivelyAddElementToBeginningOfArray(array, element) { 
-  var array = [element, ...array[1]]
+  var array = [element, ...array]
 }
